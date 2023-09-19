c6d4144f05ff1545c9eb87a88db31e137e947653
Dash2Disha <d14.dash@gmail.com>
2023-09-19 12:19:10 +0530

    changed

--- src/land02_2.js
+++ src/land02_2.js
@@ -18,13 +18,22 @@ const Land02_2 = () => {
         <div className='page02_2bg'>
         <div className='row'>
         <div className='col-4'>
-            <img src={cd1} className='cdimg1'></img></div>
+            <img src={cd1} className='cdimg1 vert-move'></img></div>
         <div className='col-4'>
-            <img src={cd2} className='cdimg2'></img></div>
+        <img src={cd2} className='cdimg2'></img></div>
         <div className='col-4'>
             <img src={cd3} className='cdimg3'></img></div>
         </div>   
+        <div className='row'>
+        <div className='col-4'>
+            <img src={cd4} className='cdimg4'></img></div>
+        <div className='col-4'>
         </div>
+        <div className='col-4'>
+            <img src={cd5} className='cdimg5 vert-move'></img></div>
+        </div>  
+        </div>
+        <h4>Best rated design tool in G2 and loved by world’s best designers</h4>
         </div>
     );
 }
