6f441c8e9ea24b07ea0007554fbc69b332df9cf7
Dash2Disha <d14.dash@gmail.com>
2023-09-20 23:43:22 +0530

    page04_5 completed

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ import Land04_1 from './land04_1';
 import Land04_2 from './land04_2';
 import Land04_3 from './land04_3';
 import Land04_4 from './land04_4';
+import Land04_5 from './land04_5';
 import Land1 from "./images/land01.png";
 import Land2 from "./images/land02.png";
 import Land3 from "./images/land03.png";
@@ -80,6 +81,7 @@ function App() {
        <Land04_2></Land04_2>
        <Land04_3></Land04_3>
        <Land04_4></Land04_4>
+       <Land04_5></Land04_5>
        
 
 
---
+++ src/land04_5.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "./land04_5.css";
+import dkimg from "./images/macbookpro.png";
+
+
+
+
+const Land04_5 = () => {
+    return(
+        <div className='land04_5'>
+            <h1>Less Clicks, More Build</h1>
+            <p>Say goodbye to lost in tons of tasks and hello to productive management with our user-friendly platform.</p>
+        
+         <img src={dkimg}></img>
+         </div>
+    );
+}
+export default Land04_5;
