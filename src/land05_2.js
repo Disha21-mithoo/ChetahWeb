900a8a25bf442253d08575d3003e8aba1438fbe0
Dash2Disha <d14.dash@gmail.com>
2023-09-21 21:21:53 +0530

    62

--- src/land05_2.js
+++ src/land05_2.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import "./land05_2.css";
-
+import hh from "./images/table.png";
 
 
 
@@ -26,13 +26,75 @@ const Land05_2 = () => {
 
 			<div class="tab-content ">
 			  <div class="tab-pane active" id="1">
-          <h3>Standard tab panel created on bootstrap using nav-tabs</h3>
+          <div className='row'>
+            <div className='col-lg-6 col-sm-12 hhbg' >
+               <img src={hh} className='hh'></img>
+            </div>
+            <div className='col-lg-6 col-sm-12 col2-05' >
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='card'>
+                            <h3>Manage Files</h3>
+                            <p>Create an on-brand home for your product and save countless hours on design time. Use this table to compare your product competitor and save.</p>
+                        </div>
+                    </div>
+                    <div className='col-12'>
+                    <div className='card'>
+                            <h3>Sort by Order</h3>
+                            <p>Create an on-brand home for your product and save countless hours on design time.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+
 				</div>
 				<div class="tab-pane" id="2">
-          <h3>Notice the gap between the content and tab after applying a background color</h3>
+                <div className='row'>
+            <div className='col-lg-6 col-sm-12 hhbg' >
+               <img src={hh} className='hh'></img>
+            </div>
+            <div className='col-lg-6 col-sm-12 col2-05' >
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='card'>
+                            <h3>Customise Files</h3>
+                            <p>Create an on-brand home for your product and save countless hours on design time. Use this table to compare your product competitor and save.</p>
+                        </div>
+                    </div>
+                    <div className='col-12'>
+                    <div className='card'>
+                            <h3>Sort by Order</h3>
+                            <p>Create an on-brand home for your product and save countless hours on design time.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
 				</div>
         <div class="tab-pane" id="3">
-          <h3>add clearfix to tab-content (see the css)</h3>
+        <div className='row'>
+            <div className='col-lg-6 col-sm-12 hhbg' >
+               <img src={hh} className='hh'></img>
+            </div>
+            <div className='col-lg-6 col-sm-12 col2-05' >
+                <div className='row'>
+                    <div className='col-12'>
+                        <div className='card'>
+                            <h3>Release Files</h3>
+                            <p>Create an on-brand home for your product and save countless hours on design time. Use this table to compare your product competitor and save.</p>
+                        </div>
+                    </div>
+                    <div className='col-12'>
+                    <div className='card'>
+                            <h3>Sort by Order</h3>
+                            <p>Create an on-brand home for your product and save countless hours on design time.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
 				</div>
 			</div>
   </div>
